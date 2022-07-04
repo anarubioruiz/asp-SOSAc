ce9adde1318b087e32d7bc22e73bec854e82bf75
Ruben.Cantarero <Ruben.Cantarero@uclm.es>
2022-07-04 09:38:35 +0200

    Docs new metadata

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ASP-Scott'
+copyright = '2022, ARCO Group'
+author = 'Ana Rubio/Ruben Cantarero'
 
 release = '0.1'
 version = '0.1.0'
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -2,10 +2,3 @@ Welcome to ASP-scott's documentation!
 ===================================
 
 Prueba de concepto
-
-.. toctree::
-   .. :maxdepth: 2
-   :caption: Contents:
-   
-   core
-   kb
