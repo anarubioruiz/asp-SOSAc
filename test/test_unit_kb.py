67244c79d4a324e7984e6185afa30a020db281ad
Ana Rubio <Ana.Rubio@uclm.es>
2022-12-28 18:56:48 +0100

    add light sensor knowledge

--- test/test_unit_kb.py
+++ test/test_unit_kb.py
@@ -1226,3 +1226,153 @@ class DoorSensor(TestCase, ScottClingo):
         )
 
         self.assertCountEqual(expected, query)
+
+
+class LightSensor(TestCase, ScottClingo):
+    def setUp(self):
+        self.clingo_setup(
+            'src/sosa_engine.lp',
+            'src/engine.lp',
+            'src/kb/sensor.lp',
+            'src/kb/observation.lp',
+        )
+
+        facts = FactBase([
+            terms.Device(
+                id='light_sensor01',
+                klass='_lightSensor_'),
+            terms.x_is_the_y_of_z(
+                value='kitchen',
+                property='location',
+                entity='light_sensor01')
+        ])
+
+        self.load_knowledge(facts)
+
+    def test_is_a_sensor(self):
+        solution = self.get_solution()
+
+        query = list(solution
+            .query(terms.Sensor)
+            .where(terms.Sensor.id == 'light_sensor01')
+            .all()
+        )
+
+        self.assertEqual(len(query), 1)
+
+    def test_observes_illuminance(self):
+        solution = self.get_solution()
+
+        expected = [
+            terms.observes(
+                sensor='light_sensor01',
+                observable_property='illuminance')
+        ]
+
+        query = list(solution
+            .query(terms.observes)
+            .all()
+        )
+
+        self.assertCountEqual(expected, query)
+
+    def test_makesObservation_of_the_lightEnergy_klass(self):
+        solution = self.get_solution()
+
+        expected = [
+            terms.makesObservation(
+                sensor='light_sensor01',
+                observation=terms.ActID(
+                    device='light_sensor01',
+                    act='lightEnergy')
+                )
+        ]
+
+        query = list(solution
+            .query(terms.makesObservation)
+            .all()
+        )
+
+        self.assertEqual(expected, query)
+
+    def test_lightEnergy_observation_observedProperty_is_illuminance(self):
+        solution = self.get_solution()
+
+        expected = [
+            terms.observedProperty(
+                observation=terms.ActID(
+                    device='light_sensor01',
+                    act='lightEnergy'),
+                observable_property='illuminance')
+        ]
+
+        query = list(solution
+            .query(terms.observedProperty)
+            .all()
+        )
+
+        self.assertCountEqual(expected, query)
+
+    def test_isHostedBy_its_location_by_default(self):
+        solution = self.get_solution()
+
+        expected = [
+            terms.isHostedBy(
+                hosted='light_sensor01',
+                platform='kitchen')
+        ]
+
+        query = list(solution
+            .query(terms.isHostedBy)
+            .all()
+        )
+
+        self.assertCountEqual(expected, query)
+
+    def test_location_is_the_observation_featureOfInterest(self):
+        solution = self.get_solution()
+
+        expected = [
+            terms.hasFeatureOfInterest(
+                act=terms.ActID(device='light_sensor01', act='lightEnergy'),
+                feature_of_interest='kitchen')
+        ]
+
+        query = list(solution
+            .query(terms.hasFeatureOfInterest)
+            .all()
+        )
+
+        self.assertCountEqual(expected, query)
+
+    def test_illuminance_is_a_location_property(self):
+        solution = self.get_solution()
+
+        expected = [
+            terms.hasProperty(
+                feature_of_interest='kitchen',
+                property='illuminance')
+        ]
+
+        query = list(solution
+            .query(terms.hasProperty)
+            .all()
+        )
+
+        self.assertCountEqual(expected, query)
+
+    def test_lightEnergy_observation_hasResult_true(self):
+        solution = self.get_solution()
+
+        expected = [
+            terms.hasResult(
+                act=terms.ActID(device='light_sensor01', act='lightEnergy'),
+                result='number')
+        ]
+
+        query = list(solution
+            .query(terms.hasResult)
+            .all()
+        )
+
+        self.assertCountEqual(expected, query)
